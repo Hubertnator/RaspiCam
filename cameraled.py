b9d3144e642dd4de8bdc1bd1263171c29aa18a35
Hubert S <41962554+Hubertnator@users.noreply.github.com>
2020-02-29 19:20:25 +0200

    Fix for gnd and other compat issues

--- cameraled.py
+++ cameraled.py
@@ -1,58 +1,57 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 LED_SINGLE_CHANNEL = "4fbc5e3c9838c7427e1bceba6bf61e52"
 LED_RGB = "31b5b427135c4c95ef5c1467964b0a61"
 # ALL = 5
+COMPAT = 0
 SINGLE = 1
 RED = 2
 GREEN = 3
 BLUE = 4
 #DUMMY GPIO CLASS
-class GPIO:
-    LOW = 0
-    HIGH = 1
-    def __init__(self):
-        pass
-    def output(a,b):
-        pass
-    def cleanup():
-        pass
+if COMPAT==True:
+    class GPIO:
+        LOW = 0
+        HIGH = 1
+        def __init__(self):
+            pass
+        def output(a,b):
+            pass
+        def cleanup():
+            pass
 
 class CameraLED:
     def __init__(self,led_type):
+        GPIO.setmode(GPIO.BCM)
         self.on = False
         self.mode = 0
         
         if(led_type == LED_SINGLE_CHANNEL):
             self.type = LED_SINGLE_CHANNEL
             self.color_pin = 0
-            self.gnd = 0
         elif(led_type == LED_RGB):               
             self.type = LED_RGB
             self.red_pin = 0
             self.green_pin = 0
             self.blue_pin = 0
-            self.gnd = 0
         else:
             print("Error #1. Initializing cameraLED failed - Unknown LED type")
     def CleanUp(self):
         pass
-    def Bind_SINGLE_CHANNEL(self,plus,gnd):
+    def Bind_SINGLE_CHANNEL(self,plus):
         self.color_pin = plus
-        self.gnd = gnd
         print(self.type,"  is bound")
         
-    def Bind_RGB(self,gnd,r,g,b):
+    def Bind_RGB(self,r,g,b):
         self.red_pin = r
         self.green_pin = g
         self.blue_pin = b
-        self.gnd = gnd
         print(self.type,"  is bound outaside")
         
-    def Bind(self,gnd,r,g=0,b=0):
+    def Bind(self,r,g=0,b=0):
         if(self.type == LED_SINGLE_CHANNEL):
-            self.Bind_SINGLE_CHANNEL(r,gnd)
+            self.Bind_SINGLE_CHANNEL(r)
         elif(self.type == LED_RGB):
-            self.Bind_RGB(r,g,b,gnd)
+            self.Bind_RGB(r,g,b)
         else:
             print("Error #3 Function Bind() does not accept given argument!")
     def On(self, mode):
@@ -105,3 +104,4 @@ led.On(RED)
 led.Unbind()
 led.CleanUp()
 del led
+
