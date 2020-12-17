f642b9f7d82f40eae464618694101a6e6a654399
Hubert S <41962554+Hubertnator@users.noreply.github.com>
2020-12-17 13:59:12 +0200

    Rewrite the LED to the CameraLED class.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import tkinter as tk
 import time
 from camera import *
+from cameraled import *
 
 
 #--initializing, creating and configuring main window
@@ -11,8 +12,14 @@ from camera import *
 rotated = True; #this will be in a config file.
 mp4_command = "MP4Box -add temp.h264 monitoring.mp4"
 
-print("Type in length of video:")
-length = int(input())
+red = 17
+green = 27
+blue = 22
+
+length = int(input("Type in length of video: "))
+while length <= 0:
+    print('Invalid duration of video!')
+    length = int(input("Type in length of video: "))
 
 config = open("general.conf")
 print(config.read())
@@ -20,22 +27,16 @@ print(config.read())
 #setting camera diode:
 GPIO.setmode(GPIO.BCM)
 
-red = 17
-green = 27
-blue = 22
+led = CameraLED(LED_RGB)
+led.Bind(red,green,blue)
 
-GPIO.setup(red, GPIO.OUT)
-GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
-
-RED = GPIO.PWM(red, 1)
-GREEN = GPIO.PWM(green, 1)
 BLUE = GPIO.PWM(blue, 1)
-
+ 
 sleep(2)
 
-RED.start(100)
 #film from raspberry pi in a for loop
+led.On(RED)
 
 camera = Camera()
 camera.Resolution(640,480)
@@ -49,11 +50,12 @@ camera.ConvertVideo("temp.h264","monitoring.mp4")
 conversion_stop = time.time()
 
 #Conversion complete, cleanup!
+del camera
 
-RED.start(0)
-GREEN.start(0)
 BLUE.start(0)
 GPIO.cleanup()
+
+del led
 
 
 print("Conversion complete, safe to exit program :)")
